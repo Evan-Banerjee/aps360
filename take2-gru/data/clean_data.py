ec6012a1dbbbb4b8f99914b8a03b5665b6ad3eb5
Noah Monti <noahamonti@gmail.com>
2024-11-21 12:15:56 -0500

    Updated data cleaner

--- take2-gru/data/clean_data.py
+++ take2-gru/data/clean_data.py
@@ -7,9 +7,14 @@ def clean_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as f, open(file_name + '-cleaned-' + file_path, 'w') as f_out:
 
         for line in f:
+            line = line.replace('\r', '')
+            line = line.replace('\n\n', '')
+            line = re.sub(r'-', ' ', line)
+            line = re.sub(r'\n[ \t]+', '\n', line)
+            line = re.sub(r'[ \t]+', ' ', line)
             line = re.sub(r'-', ' ', line)
             cleaned_line = re.sub(r"[^a-zA-Z\s']", '', line)
             # Write the cleaned line to the output file
             f_out.write(cleaned_line)
 
-clean_data('morepoems.txt')
+clean_data('poems.txt')
